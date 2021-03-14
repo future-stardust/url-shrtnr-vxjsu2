fc308eff9541b5f3b790a282d934777ddfd7cec8
413x <dedifferentiator@protonmail.ch>
2021-03-14 23:20:19 +0200

    refactor: add greeting message

--- src/Server/Server.hs
+++ src/Server/Server.hs
@@ -44,4 +44,23 @@ app ctx = do
   return . serveWithContext api cfg $ server ctx jwtCfg cookieCfg
 
 up :: AppCtx -> IO ()
-up ctx = run 8080 =<< app ctx
+up ctx = do
+  let port = 8080
+
+  putTextLn "            _nnnn_"
+  putTextLn $ "           dGGGGMMb     ," <> toText (['\"'| _ <- [0..34]]) <> "."
+  putTextLn $ "          @p~qp~~qMb    | Running at http://localhost:" <> show port <> "/ | "
+  putTextLn "          M|@||@) M|   _;...................................'"
+  putTextLn "          @,----.JM| -'"
+  putTextLn "         JS^\\__/  qKL"
+  putTextLn "        dZP        qKRb"
+  putTextLn "       dZP          qKKb"
+  putTextLn "      fZP            SMMb"
+  putTextLn "      HZM            MMMM"
+  putTextLn "      FqM            MMMM"
+  putTextLn "    __| \".        |\\dS\"qML"
+  putTextLn "   |    `.       | `' \\Zq"
+  putTextLn "  _)      \\.___.,|     .'"
+  putTextLn "  \\____   )MMMMMM|   .'"
+  putTextLn "       `-'       `--'\""
+  run port =<< app ctx
