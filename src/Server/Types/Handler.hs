373622d61d8692664f8e41cfaf289f65be171655
persik <yroslav541@gmail.com>
2021-03-15 19:05:10 +0200

    Update src/Server/Types/Handler.hs
    Co-authored-by: 413x <dedifferentiator@protonmail.ch>

--- src/Server/Types/Handler.hs
+++ src/Server/Types/Handler.hs
@@ -12,6 +12,6 @@ import           Relude
 -- | Custom Handler monad
 type HandlerT = ReaderT AppCtx Handler
 
--- | Used to convert custom HandlerT to the Servant Handler
+-- | Convert custom HandlerT to Servant Handler
 toH :: AppCtx -> HandlerT b -> Handler b
 toH = flip runReaderT
